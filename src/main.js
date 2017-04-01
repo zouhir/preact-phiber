d3fb6ebd8767cfd0b45f5f7286a3b82fb6db173d
Zouhir Chahoud <zouhir@zouhir.org>
2017-04-01 23:38:58 +1100

    initial 🚀

---
+++ src/main.js
@@ -0,0 +1,42 @@
+require('./style.scss');
+
+var OfflinePlugin = require('offline-plugin/runtime');
+
+OfflinePlugin.install({
+  onInstalled: function() {
+    openOfflineReady();
+  },
+
+  onUpdating: function() {
+
+  },
+
+  onUpdateReady: function() {
+    OfflinePlugin.applyUpdate();
+  },
+  onUpdated: function() {
+    window.location.reload();
+  }
+});
+
+
+(function() {
+  var loaded = false; 
+  document.addEventListener('readystatechange', function() {
+      if (!loaded && (document.readyState === 'interactive' || document.readyState === 'complete')) {
+        loaded = true;
+        createVideo()
+      }
+    },
+    false
+  );
+ 
+  function createVideo () {
+    var videoEl = document.createElement("video");
+    var sourceMP4 = document.createElement("source"); 
+    sourceMP4.type = "video/mp4";
+    sourceMP4.src = "/demo.mp4";
+    videoEl.appendChild(sourceMP4);
+    document.getElementById('video').appendChild(videoEl); 
+  }
+}());
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const OfflinePlugin = require('offline-plugin');
+
+module.exports = {
+  context: path.resolve('src'),
+  entry: './main.js',
+  output: {
+    path: path.resolve('dist'),
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function() {
+                  return [
+                    require('autoprefixer')({ browsers: ['> 1%', 'IE >= 10'] })
+                  ];
+                }
+              }
+            },
+            {
+                loader: "sass-loader",
+                options: {
+                    sourceMap: true
+                }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(woff2?|ttf)$/,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loaders: ['raw-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'favicons', from: 'media' }
+    ]),
+    new OfflinePlugin({
+      safeToUseOptionalCaches: true,
+
+      caches: {
+        main: [
+          'main.js',
+          'main.css',
+          'index.html'
+        ],
+        additional: [
+          '*.woff',
+          '*.woff2'
+        ],
+        optional: [
+          ':rest:'
+        ]
+      },
+
+      ServiceWorker: {
+        events: true
+      },
+      AppCache: {
+        events: true
+      }
+    })
+  ]
+};
