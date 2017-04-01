ab296bb3be4a89d6c96c169671f6108c6ebf9869
Zouhir Chahoud <zouhir@zouhir.org>
2017-04-02 01:23:46 +1100

    video bugfix

--- src/main.js
+++ src/main.js
@@ -21,23 +21,13 @@ OfflinePlugin.install({
 
 
 (function() {
-  var loaded = false; 
-  document.addEventListener('readystatechange', function() {
-      if (!loaded && (document.readyState === 'interactive' || document.readyState === 'complete')) {
-        loaded = true;
-        createVideo()
-      }
-    },
-    false
-  );
- 
-  function createVideo () {
-    var videoEl = document.createElement("video");
-    var sourceMP4 = document.createElement("source"); 
-    sourceMP4.type = "video/mp4";
-    sourceMP4.src = "/demo.mp4";
-    videoEl.setAttribute("controls", "true");
-    videoEl.appendChild(sourceMP4);
-    document.getElementById('video').appendChild(videoEl); 
-  }
+
+  var videoEl = document.createElement("video");
+  var sourceMP4 = document.createElement("source"); 
+  sourceMP4.type = "video/mp4";
+  sourceMP4.src = "/demo.mp4";
+  videoEl.setAttribute("controls", "true");
+  videoEl.appendChild(sourceMP4);
+  document.getElementById('video').appendChild(videoEl); 
+
 }());
